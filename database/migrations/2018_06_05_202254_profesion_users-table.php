b4a0f1d0fabc51dd472881a1bbe8a82e2c0c8091
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-06-05 15:44:22 -0500

    ajustes de migraciones en base de datos

---
+++ database/migrations/2018_06_05_202254_profesion_users-table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProfesionUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('Profesion', function (Blueprint $table){
+            $table->increments('codigo');
+            $table->string('titulo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Profesion');
+    }
+}
