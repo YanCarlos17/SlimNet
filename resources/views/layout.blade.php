9b7374b3966eaa127e3b21ccc3ad800da8332bdd
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-06-07 10:25:09 -0500

    se cambia el layout por un diseño propio

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,81 +1,56 @@
-{{-- HEADER --}}
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Layout</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+  {{-- HEADER --}}
+  <div class="header">
+    <div class="header__logo">
 
-    <title> @yield('title') - SlimNet</title>
+    </div>
+    <div class="socials">
+        <p class="social__text">
+          Follow me
+        </p>
+      <a href="#" class="redes"><span class="icon">&</span></a>
+      <a href="#" class="redes"><span class="icon">&</span></a>
+      <a href="#" class="redes"><span class="icon">&</span></a>
+    </div>
+  </div>
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  {{-- BANNER --}}
+  <div class="banner">
+    <div class="carousel">
 
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('css/sticky-footer-navbar.css') }}" rel="stylesheet">
-  </head>
+    </div>
+  </div>
 
-  <body>
+  {{-- NAVBAR --}}
+  <div class="navbar">
+      <ul class="navbar__list">
+      <a class="list__item" href="{{url('/')}}"><li>1</li></a>
+      <a class="list__item" href=""><li>2</li></a>
+      <a class="list__item" href=""><li>3</li></a>
+      <a class="list__item" href=""><li>4</li></a>
+      <a class="list__item" href=""><li>5</li></a>
+    </ul>
+  </div>
 
-    <header>
-      <!-- Fixed navbar -->
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top barnav">
-        <a class="navbar-brand" href="#"><figure class="headlogo"><img src="{{ asset('img/logo.png') }}" alt=""></figure></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('/home') }}">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Quienes somos?</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="#">Portafolio</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-          </form>
-        </div>
-      </nav>
-    </header>
+  {{-- CONTAINT --}}
+  <div class="container">
 
-    {{-- MAIN --}}
-    <main role="main" class="content">
+  </div>
 
-        <div class="row mt-3">
-            {{-- CONTENT --}}
-            <div class="col-9">
-                @yield('content')
-            </div>
-            {{-- SIDE-BAR --}}
-            <div class="col-3">
-                @section('sidebar')
-                    <h2>Barra Lateral</h2>
-                @show    
-            </div>
-        </div>
-        
-    </main>
+  {{-- FOOTER --}}
+  <div class="footer">
+    <footer>
 
-    {{-- FOOTER --}}
-    <footer class="footer">
-        <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
-        </div>
     </footer>
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    </body>
+  </div>
+
+  </body>
 </html>     
